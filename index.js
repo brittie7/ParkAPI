280aefbd37982dc62928285f96a2c61f2c96f025
Brittany Coleman <brittie7@gmail.com>
2020-09-13 06:22:14 -0400

    search in multiple states

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 'use strict'
-const apiKey = 'QJfBOs2opfEhsBw5iNv8PFFBEbAIe3GfiUbwa6ia'; 
+const apiKey = 'QJfBOs2opfEhsBw5iNv8PFFBEbAIe3GfiUbwa6ia';
 const searchURL = 'https://developer.nps.gov/api/v1/parks'
 
 function formatQueryParams(params) {
@@ -9,15 +9,15 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function getParks (query, maxResults=10){
+function getParks(query, maxResults = 10) {
   console.log('getParks ran');
-    const params = {
-      api_key: apiKey,
-      stateCode: query,
-      limit: maxResults
-    }; 
-    const queryString = formatQueryParams(params)
-    const url = searchURL + '?' + queryString;  
+  const params = {
+    api_key: apiKey,
+    stateCode: query,
+    limit: maxResults
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
   console.log(url);
 
   fetch(url)
@@ -33,33 +33,37 @@ function getParks (query, maxResults=10){
     });
 }
 
-function displayResults(responseJson){
+function displayResults(responseJson) {
   console.log("hello world")
   console.log(responseJson);
   $('#results-list').empty();
 
-  for (let i = 0; i < responseJson.data.length; i++){
+  for (let i = 0; i < responseJson.data.length; i++) {
     $('#results-list').append(
       `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].name}</a></h3><p>${responseJson.data[i].description}</p>
       </li>`
-    )};
+    )
+  };
   //display the results section  
   $('#results').removeClass('hidden');
 }
 
 
 function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-      console.log('WatchForm Ran')
-      const searchTerm = $('#js-search-term').val();
-      const maxResults = $('#js-max-results').val();
-      console.log(searchTerm, maxResults);
-      getParks(searchTerm, maxResults);
-    });
-  }
-  
-  $(watchForm);
+  $('form').submit(event => {
+    event.preventDefault();
+    console.log('WatchForm Ran')
+    const searchTerm = ($('#js-search-term').val()).split(',');
+
+
+    console.log(typeof (searchTerm));
+    const maxResults = $('#js-max-results').val();
+    console.log(searchTerm, maxResults);
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
 
   // https://api.github.com/users/brittie7/repos
   // just building something that adds
